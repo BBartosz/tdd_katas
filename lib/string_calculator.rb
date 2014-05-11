7088b997bb565b220945e54ac9a0249f78a8badc
BBartosz <bbartek91@gmail.com>
2014-05-12 01:12:03 +0200

    changed method sum_of to private

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,15 +1,16 @@
 class StringCalculator
   def add(string)
     if string.include?(',')
-      sum_of(string)
+      return sum_of(string)
     else
       return string.to_i if !string.include?(',')
     end
   end
 
+  private
+
   def sum_of(string)
     array_of_numbers = string.split(',').map { |n| n.to_i }
     sum = array_of_numbers.reduce(:+)
-    return sum
   end
 end
